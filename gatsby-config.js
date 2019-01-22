489957993b58577cb6d8f60603533b74805f6208
Filippo Rivolta <rivoltafilippo@gmail.com>
2019-01-22 11:30:03 +0100

    Setting up blog listing page query

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,69 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: "68.183.75.94",
+        // The protocol. This can be http or https.
+        protocol: "http",
+        hostingWPCOM: false,
+        useACF: true,
+        // Include specific ACF Option Pages that have a set post ID
+        // Regardless if an ID is set, the default options route will still be retrieved
+        // Must be using V3 of ACF to REST to include these routes
+        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
+        // routes with the ID option_page_1 and option_page_2
+        // Dashes in IDs will be converted to underscores for use in GraphQL
+        acfOptionPageIds: [],
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          htaccess_user: "",
+          htaccess_pass: "",
+          htaccess_sendImmediately: false,
+        },
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems.
+        verboseOutput: true,
+        // Set how many pages are retrieved per API request.
+        perPage: 100,
+        // Search and Replace Urls across WordPress content.
+        searchAndReplaceContentUrls: {
+          sourceUrl: "http://68.183.75.94",
+          replacementUrl: "https://www.filipporivolta.it",
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "**/*/*/categories",
+          "**/*/*/posts",
+          "**/*/*/pages",
+          "**/*/*/media",
+          "**/*/*/tags",
+          "**/*/*/taxonomies",
+          "**/*/*/users",
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: ["**/*/*/posts/1456"],
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
   ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,59 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { Link, graphql } from 'gatsby'
+import { Container, Row, Col } from 'reactstrap';
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const SecondPage = () => (
-  <Layout>
-    <p>Blog page</p>
-  </Layout>
-)
+class Blog extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Container>
+          <Row>
+            <Col>
+              {this.props.data.allWordpressPost.edges.map((post)=>(
+                <div className="Post">
+                  <Container>
+                    <Row>
+                      <Col>
+                        <h2 className="Post__title"><Link className="button is-small" to={post.node.slug}>{post.node.title}</Link></h2>
+                        <div
+                           dangerouslySetInnerHTML={{
+                            __html: post.node.excerpt.replace(/<p class="link-more.*/, ''),
+                          }}
+                        />
 
-export default SecondPage
+                        <small className="Post__date">{post.node.date}</small>
+                      </Col>
+                    </Row>
+                  </Container>
+                </div>
+              ))}
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
+    )
+  }
+}
+
+
+export default Blog
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPost {
+      edges {
+        node {
+          id
+          slug
+          title
+          content
+          excerpt
+          date
+          modified
+        }
+      }
+    }
+  }
+`
