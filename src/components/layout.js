e68bd352e67c97d920f9bb48cff1af03dc1a660c
Filippo Rivolta <rivoltafilippo@gmail.com>
2019-01-28 10:26:48 +0100

    GA

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,9 @@ import Contact from '../components/containers/contacts/Contacts'
 import Header from './containers/header'
 import '../styles/main.scss'
 import ShoutBar from './commons/shoutbar';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-133277122-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const Layout = ({ children }) => (
   <div>
