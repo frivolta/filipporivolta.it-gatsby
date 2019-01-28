281b163c5f2cc195ff7e9bd793c27920efc76df0
Filippo Rivolta <rivoltafilippo@gmail.com>
2019-01-28 10:12:05 +0100

    GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,5 +93,20 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-133277122-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
   ],
 }
