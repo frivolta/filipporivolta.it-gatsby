4502fb431acdc3ed85ce1913ddbf5aacecd3c51e
Filippo Rivolta <rivoltafilippo@gmail.com>
2019-02-05 16:59:57 +0100

    Add language switcher

--- src/components/containers/header.js
+++ src/components/containers/header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
 import { Button } from 'reactstrap'
 import { Logo } from './../commons/logo'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import InstagramImg from '../../images/social/instagram.svg'
+import LanguageSwitcher from '../commons/languageSwitcher';
 /* import BurgerMenu from '../commons/burger-menu';
  */
 const Header = ({locale, callToAction}) => (
@@ -12,11 +14,12 @@ const Header = ({locale, callToAction}) => (
             <div className="container__wide Header__container">
                 <Logo logoWidth="180" locale={locale}/>
                 <div className="Header__right">
-                    <AnchorLink href='#contact'><Button color="primary" className="contact-button d-none d-sm-block" >{callToAction}</Button></AnchorLink>
+                    {/*<AnchorLink href='#contact'><Button color="primary" className="contact-button d-none d-sm-block" >{callToAction}</Button></AnchorLink>*/}
                     {/* <BurgerMenu/> */}
-                    <a href="https://www.instagram.com/filippo_dot_it/" target="_blank">
+                    <a href="https://www.instagram.com/filippo.jsx/" target="_blank">
                         <img className="social-button" src={InstagramImg} alt="Instagram Filippo Rivolta Social"/>
                     </a>
+                    <LanguageSwitcher locale={locale}/>
                 </div>
             </div>
         </div>
